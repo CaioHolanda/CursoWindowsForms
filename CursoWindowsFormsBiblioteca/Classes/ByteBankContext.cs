7936ed58b1c3491809cfadfe1dd9c0305ba758e7
CaioHolanda <caiojholanda@gmail.com>
2023-12-28 19:00:26 +0000

    Add project files.

---
+++ CursoWindowsFormsBiblioteca/Classes/ByteBankContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+
+namespace CursoWindowsFormsBiblioteca.Classes
+{
+    public class ByteBankContext : DbContext
+    {
+        public DbSet<Cliente> TB_Cliente { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=CAIOLAPTOP;Initial Catalog=ByteBank;Persist Security Info=True;User ID=sa;Password=123;Encrypt=False");
+
+        }
+    }
+}
